97eefc2d8db820dc30f00479d6eee1e1ba9d294b
aidant842 <aidant842@gmail.com>
2020-05-20 15:31:02 +0000

    started basics, added static and templates folder, added app.py setup mongodb  and first route decorator, added env.py and gitignore file, a basic page with nav element and background image

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 import os
-from flask import Flask
+if os.path.exists("env.py"):
+   import env
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
+
+app.config['MONGO_DBNAME'] = 'task_manager'
+app.config['MONGO_URI'] = os.getenv('MONGO_URI')
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return 'Hello world'
+@app.route('/home_page')
+def home_page():
+    return render_template('index.html', games=mongo.db.games.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
