aa342472cb2f10ccbabc424163e1a1529077418c
aidant842 <aidant842@gmail.com>
2020-05-21 09:22:33 +0000

    added back if os.path.exists into app.py due to no module named env error

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from os import path
-import env
+if os.path.exists("env.py"):
+   import env
 
 app = Flask(__name__)
 
