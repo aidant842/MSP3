238e278e5bf8bb759ea09d1dcac62de7dffd675c
aidant842 <aidant842@gmail.com>
2020-05-21 08:25:21 +0000

    possibly fixed  application error caused by failing of getting mongo uri correctly

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import os
-if os.path.exists("env.py"):
-   import env
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+  import env 
 
-app = Flask(__name__, static_url_path='/static')
+app = Flask(__name__)
 
 app.config['MONGO_DBNAME'] = 'task_manager'
-app.config['MONGO_URI'] = os.getenv('MONGO_URI')
+app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
 
 mongo = PyMongo(app)
 
