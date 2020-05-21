a2d39370038c7d4c61e1d10f1c2542e1dba371ea
aidant842 <aidant842@gmail.com>
2020-05-21 08:45:25 +0000

    corrected mongo db name

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from os import path
 if path.exists("env.py"):
-  import env 
+    import env
 
 app = Flask(__name__)
 
-app.config['MONGO_DBNAME'] = 'task_manager'
+app.config['MONGO_DBNAME'] = 'gameDB'
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
 mongo = PyMongo(app)
