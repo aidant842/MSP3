5e667f3febf656760dcf9b0c11f8758ae2490130
aidant842 <aidant842@gmail.com>
2020-05-21 09:14:28 +0000

    tried importing env without if statement in app.py, and re-added uri into env.py

--- app.py
+++ app.py
@@ -3,13 +3,12 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from os import path
-if path.exists("env.py"):
-    import env
+import env
 
 app = Flask(__name__)
 
 app.config['MONGO_DBNAME'] = 'gameDB'
-app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")
 
 mongo = PyMongo(app)
 
